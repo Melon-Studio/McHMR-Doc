b914db31c9cfe73f1d07b477119eb6c55440b24f
Gabriel Ryder <xiaofan6@foxmail.com>
2024-09-03 16:42:29 +0800

    Update docs

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vitepress'
 import { sharedConfig } from './shared'
+import { pagefindPlugin } from 'vitepress-plugin-pagefind'
 
 export default defineConfig({
     ...sharedConfig,
+    vite: {
+        plugins: [pagefindPlugin()],
+    }
 })
